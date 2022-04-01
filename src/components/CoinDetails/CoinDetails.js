b418289d26e46b65580d31dfced73bf78d5a0ac7
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-01 19:00:15 +0600

    coins detail complete

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 const CoinDetails = () => {
     const { id } = useParams()
     const [coin, setCoin] = useState([])
+    console.log(coin);
 
     useEffect(() => {
         fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
@@ -13,12 +14,23 @@ const CoinDetails = () => {
     }, [id])
 
     return (
-        <div className='px-4 pb-24 mx-auto max-w-7xl md:px-2'>
+        <div className='h-{80vh} px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
             <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 content-centr'>
                 <div >
                     <h1 className='text-3xl'> Genarel info:</h1>
                     <hr />
-                    <h2>Name{coin.name}</h2>
+                    <h2>Coin name{coin.name}</h2>
+                    <h2>market cap rank:{coin.market_cap_rank}</h2>
+                    <h2>Origin{coin.country_origin ? coin.country_origin : 'Not found'}</h2>
+                    <h2>Coin name{coin.name}</h2>
+
+
+                    <h1 className='text-3xl'> Scores:</h1>
+                    <hr />
+                    <h2>Community Score :{coin.community_score}</h2>
+                    <h2>Developer Score:{coin.market_cap_rank}</h2>
+                    <h2>Liquidity Score:{coin.country_origin ? coin.country_origin : 'Not found'}</h2>
+                    <h2>Public Interest Score:{coin.public_interest_score}</h2>
                 </div>
 
                 <div>
